13e53be231686f404c06859ea2b0cbeb3201971f
fkaaziebu <frederickaziebu1998@gmail.com>
2024-02-09 19:59:06 +0000

    feat: build login/register pages

---
+++ components/auth/register/Register.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    class="flex flex-col gap-6 justify-center bg-white rounded-md p-5 py-10 w-[25%]"
+  >
+    <div class="flex items-center justify-center">
+      <h1 class="text-2xl font-bold">Register</h1>
+    </div>
+    <div class="flex flex-col gap-10">
+      <div class="flex flex-col gap-2">
+        <label for="email">Email</label>
+        <input
+          type="text"
+          id="email"
+          class="flex h-12 w-full rounded-md border border-gray-300 px-3 py-2 text-sm text-zinc-950 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-900 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+        />
+      </div>
+      <div class="flex flex-col gap-2">
+        <label for="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          class="flex h-12 w-full rounded-md border border-gray-300 px-3 py-2 text-sm text-zinc-950 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-900 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+        />
+      </div>
+      <div class="flex flex-col gap-2">
+        <label for="confirm_password">Confirm Password</label>
+        <input
+          type="password"
+          id="confirm_password"
+          class="flex h-12 w-full rounded-md border border-gray-300 px-3 py-2 text-sm text-zinc-950 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-900 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+        />
+        <div>
+          <NuxtLink to="/auth/login" class="text-blue-500">
+              Already have an account? Click here to Login
+          </NuxtLink>
+        </div>
+      </div>
+      <div class="flex">
+        <button
+          class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-blue-700 w-full p-3 text-blue-50 hover:bg-blue-700/90"
+        >
+          Register
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/auth/login/index.vue
+++ pages/auth/login/index.vue
@@ -1,3 +1,13 @@
+<script setup>
+definePageMeta({
+  layout: "auth",
+});
+
+import Login from "~/components/auth/login/Login.vue";
+</script>
+
 <template>
-  <div>Login page</div>
+  <div class="flex items-center justify-center bg-blue-600 w-full h-screen">
+    <Login />
+  </div>
 </template>
